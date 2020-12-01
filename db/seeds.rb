59d007162258af6c4d8128a02db1dc2c4d7ca15f
Meredith <meredith@mcintosh.io>
2020-12-01 16:08:08 -0500

    update

---
+++ db/seeds.rb
@@ -0,0 +1,25 @@
+#user data
+meredith = User.create(name:"Meredith")
+avelon = User.create(name:"Avelon")
+alec = User.create(name:"Alec")
+
+#dog data
+
+
+#shelter data
+
+#adoption data
+
+
+
+
+
+
+
+
+
+
+
+
+
+puts "Seed file ran"
